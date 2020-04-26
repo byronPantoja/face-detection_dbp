6cd3786b6db98a7dfdbf6538232fbfc4f90ceea8
dev833 <byronpantoja@gmail.com>
2020-04-26 10:31:18 +0800

    Added most of the components and styles for the front end portion of this project review. Added security for my API key via .env file and gitignor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Clarifai from 'clarifai';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation.component';
+import Logo from './components/Logo/Logo.component';
+import Rank from './components/Rank/Rank.component';
+import ImageLinkForm from './components/Image-Link-Form/ImageLinkForm.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+	apiKey: process.env.REACT_APP_CLARIFAI_DBP_API_KEY,
+});
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 50,
+			density: {
+				enable: true,
+				value_area: 800,
+			},
+		},
+		line_linked: {
+			shadow: {
+				enable: true,
+				color: '#5279ff',
+				blur: 3,
+			},
+		},
+	},
+};
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			input: '',
+		};
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	onButtonSubmit = () => {
+		console.log('click');
+		app.models
+			.predict(
+				'a403429f2ddf4b49b307e318f00e528b',
+				'https://samples.clarifai.com/face-det.jpg'
+			)
+			.then(
+				function(response) {
+					console.log(response);
+				},
+				function(err) {
+					// there was an error
+				}
+			);
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<Particles className='particles' params={particlesOptions} />
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm
+					onInputChange={this.onInputChange}
+					onButtonSubmit={this.onButtonSubmit}
+				/>
+				{/*
+
+          <FaceRecognition />
+        */}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Rank/Rank.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>{'W3 AR3 B0RG'}</div>
+			<div className='white f1'>{'R3SIST3NCE IS FUT1L3...'}</div>
+		</div>
+	);
+};
+
+export default Rank;
